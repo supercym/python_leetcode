0385bce024ba3ce03197c4928e25cfa33fd3a85d
supercym <cym467000@163.com>
2018-08-04 22:36:20 +0800

    two sum

---
+++ two_sum.py
@@ -0,0 +1,15 @@
+# Author: cym
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i, val in enumerate(nums):
+            temp = target - val
+            for j in range(i+1, len(nums)):
+                if nums[j] == temp:
+                    ans = [i, j]
+                    return ans
+        return 0
