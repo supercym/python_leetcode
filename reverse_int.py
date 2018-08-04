45d9419efee5f7a14fd7aa0ec6e6cd7900e68dbe
supercym <cym467000@163.com>
2018-08-04 22:54:10 +0800

    reverse_int_07

---
+++ reverse_int.py
@@ -0,0 +1,18 @@
+# Author: cym
+class Solution:
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        flag = 1
+        if x < 0:
+            x = -x
+            flag = -1
+        c = [str(n) for n in list(str(x))]
+        c = c[::-1]
+        d = int("".join(c))*flag
+        if (d < -(1 << 31)) or (d > (1 << 31)-1):
+            return 0
+        else:
+            return d
