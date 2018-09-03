2f4d1e06c88215d130c326306b1a07c44e75933a
supercym <cym467000@163.com>
2018-09-03 20:58:01 +0800

    leetcode 167 Two Sum II Input Array Is Sorted

---
+++ 167_Two_Sum_II_Input_Array_Is_Sorted.py
@@ -0,0 +1,22 @@
+# Author: cym
+
+class Solution:
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        left = 0
+        right = len(numbers) - 1
+
+        while left < right:
+            sum_val = numbers[left] + numbers[right]
+            if sum_val == target:
+                break
+            if sum_val < target:
+                left += 1
+            if sum_val > target:
+                right -= 1
+        return [left+1, right+1]
+    
