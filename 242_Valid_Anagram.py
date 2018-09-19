14d82243ee4e804efab2cc5a4bf96835a90ef5cf
supercym <cym467000@163.com>
2018-09-19 09:07:36 +0800

    leetcode 242 Valid Anagram

---
+++ 242_Valid_Anagram.py
@@ -0,0 +1,31 @@
+# Author: cym
+
+class Solution:
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        ds = {}
+        dt = {}
+        for i in range(len(s)):
+            try:
+                ds[s[i]] += 1
+            except:
+                ds[s[i]] = 1
+        for i in range(len(t)):
+            try:
+                dt[t[i]] += 1
+            except:
+                dt[t[i]] = 1
+        for k, v in ds.items():
+            if k not in dt.keys():
+                return False
+            if v != dt[k]:
+                return False
+        return True
+        
+
