c766f9f4b936fd4d094932eb7e85e9b3db6415ac
supercym <cym467000@163.com>
2018-09-26 09:16:24 +0800

    leetcode 326 Power of Three

---
+++ 326_Power_of_Three.py
@@ -0,0 +1,19 @@
+# Author: cym
+
+class Solution:
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n == 0:
+            return False
+        if n == 1:
+            return True
+        while n % 3 == 0:
+            n //= 3
+            if n == 1:
+                return True
+        return False
+
+
