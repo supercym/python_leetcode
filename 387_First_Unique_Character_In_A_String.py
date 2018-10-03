a06e62964c063e4719d4ddbee0de62bbb706efbd
supercym <cym467000@163.com>
2018-10-03 09:54:34 +0800

    leetcode 387 First Unique Character In A String

---
+++ 387_First_Unique_Character_In_A_String.py
@@ -0,0 +1,20 @@
+# Author: cym
+
+class Solution:
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        d = {}
+        for i, c in enumerate(s):
+            if c not in d.keys():
+                d[c] = i
+            else:
+                d[c] = len(s)
+        min_index = len(s)
+        for _, v in d.items():
+            min_index = min(min_index, v)
+        return -1 if min_index == len(s) else min_index
+
+            
