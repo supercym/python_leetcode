32b2007e12ffe0f715c6d61bb4f5273a34238f75
supercym <cym467000@163.com>
2018-10-17 20:01:52 +0800

    leetcode # Definition for a Node. class Node(object):     def __init__(self, val, children):         self.val = val         self.children = children

---
+++ 429_N_Ary_Tree_Level_Order_Traversal.py
@@ -0,0 +1,29 @@
+# Author: cym
+
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: Node
+        :rtype: List[List[int]]
+        """
+        if root is None:
+            return []
+        ans = []
+        queue = [root]
+        while len(queue):
+            queT = []
+            ansT = []
+            for node in queue:
+                for c in node.children:
+                    queT.append(c)
+                ansT.append(node.val)
+            ans.append(ansT)
+            queue = queT
+        return ans
