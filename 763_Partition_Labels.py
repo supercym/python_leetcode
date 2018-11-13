16414cfca2fada9ac3195f91d2a72923650ef92e
supercym <cym467000@163.com>
2018-11-13 21:41:28 +0800

    leetcode 763 Partition Labels

---
+++ 763_Partition_Labels.py
@@ -0,0 +1,35 @@
+# Author: cym
+
+class Solution:
+    def partitionLabels(self, S):
+        """
+        :type S: str
+        :rtype: List[int]
+        """
+        ans = []
+        while len(S):
+            k = self.partition(S)
+            S = S[k:]
+            ans.append(k)
+        return ans
+
+    def partition(self, S):
+        """
+        :type S: str
+        :rtype: List[int]
+        """
+        index = {}
+        for i in range(len(S)):
+            index[S[i]] = i
+        subStrEnd = index[S[0]]
+        pointer = 0
+        while index[S[pointer]] <= subStrEnd:
+            pointer += 1
+            if pointer > subStrEnd:
+                break
+            if index[S[pointer]] > subStrEnd:
+                subStrEnd = index[S[pointer]]
+        return pointer
+    
+
+
